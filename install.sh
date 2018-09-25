6932041b2efb462f39ad849ad454d7197422e298
lijing00333 <lijing00333@163.com>
2018-09-25 23:02:09 +0800

    本地脚本更新

--- install.sh
+++ install.sh
@@ -24,7 +24,9 @@ install() {
 }
 
 update() {
-  (cd $HOME/.vim; git pull;git submodule foreach git checkout master;)
+	(cd $HOME/.vim; git pull;)
+	(cd $HOME/.vim; git submodule update --remote;)
+	(cd $HOME/.vim; git submodule foreach git checkout master;)
   #vim +BundleClean +BundleInstall +qall! </dev/tty
 }
 
